ce3a6ca54b5adc2bf0f6eca0fba45387f2d54f74
Leandro Schillreff <leandroschillreff@gmail.com>
2024-10-03 08:26:21 -0400

    feat: footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import Layout from "./Layout";
+
+const Footer = () => {
+  return (
+    <footer
+      className="w-full border-t-2 border-solid border-dark font-medium 
+    text-lg"
+    >
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <Layout className="py-8 flex items-center justify-between">
+          <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
+          <div className="flex items-center">
+            Built With{" "}
+            <span className="text-primary text-2xl px-1">&#9825; </span>
+            by&nbsp;
+            <Link
+              href="https://github.com/schillreff"
+              target="_blank"
+              className="underline underline-offset-2"
+            >
+              Schillreff
+            </Link>
+          </div>
+          <Link
+            href="https://www.linkedin.com/in/leandroschillreff"
+            target="_blank"
+            className="underline underline-offset-2"
+          >
+            Say Hello
+          </Link>
+        </Layout>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
