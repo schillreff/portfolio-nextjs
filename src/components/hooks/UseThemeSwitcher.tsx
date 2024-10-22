8ace2ba5b44d89e124d17ad56ffecfe6cbfe46e2
Leandro Schillreff <leandroschillreff@gmail.com>
2024-10-22 11:19:53 -0400

    feat: hook for use theme switcher

---
+++ src/components/hooks/UseThemeSwitcher.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+
+const UseThemeSwitcher = () => {
+  const preferDarkQuery = "(prefer-color-scheme: dark)";
+  const [mode, setMode] = useState("");
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(preferDarkQuery);
+    const userPreference = window.localStorage.getItem("theme");
+
+    const handleChange = () => {
+      if (userPreference) {
+        let check = userPreference === "dark" ? "dark" : "light";
+        console.log("handle");
+        console.log(check);
+        setMode(check);
+        if (check === "dark") {
+          document.documentElement.classList.add("dark");
+        } else {
+          document.documentElement.classList.remove("dark");
+        }
+      } else {
+        let check = mediaQuery.matches ? "dark" : "light";
+        setMode(check);
+        if (check === "dark") {
+          document.documentElement.classList.add("dark");
+        } else {
+          document.documentElement.classList.remove("dark");
+        }
+      }
+    };
+
+    mediaQuery.addEventListener("change", handleChange);
+    return () => mediaQuery.removeEventListener("change", handleChange);
+  }, []);
+
+  useEffect(() => {
+    if (mode === "dark") {
+      window.localStorage.setItem("theme", "dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      window.localStorage.setItem("theme", "light");
+      document.documentElement.classList.remove("dark");
+    }
+  }, [mode]);
+
+  return [mode, setMode] as const;
+};
+
+export default UseThemeSwitcher;
