0b04d1e95a5efcadcf7224ab3e2388f9d9978195
Leandro Schillreff <leandroschillreff@gmail.com>
2024-10-24 09:09:36 -0400

    feat: added dark theme

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,9 @@ const Footer = () => {
           <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
           <div className="flex items-center">
             Built With{" "}
-            <span className="text-primary text-2xl px-1">&#9825; </span>
+            <span className="text-primary dark:text-primaryDark text-2xl px-1">
+              &#9825;
+            </span>
             by&nbsp;
             <Link
               href="https://github.com/schillreff"
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -42,12 +42,15 @@ export default function About() {
       <Head>
         <title>Schillreff | About Page</title>
       </Head>
-      <main className="container mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center justify-center">
+      <main
+        className="container mx-auto px-4 sm:px-6 lg:px-8 flex flex-col 
+      items-center justify-center dark:text-light"
+      >
         <Layout className="pt-16 pb-16">
           <AnimatedTitle text="Passion Fuels Purpose!" className="mb-16" />
-          <div className="grid w-full grid-cols-8 gap-16">
+          <div className="grid w-full grid-cols-8 gap-16 ">
             <div className="col-span-3 flex flex-col items-start justify-start">
-              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
                 Biography
               </h2>
               <p className="font-medium text-justify">
@@ -72,11 +75,11 @@ export default function About() {
             </div>
             <div
               className="col-span-3 relative h-max rounded-2xl border-2 
-            border-solid border-dark bg-light p-8"
+            border-solid border-dark dark:border-light bg-light dark:bg-dark p-8"
             >
               <div
                 className="absolute top-0 -right-3 -z-10 w-[calc(100%+12px)] 
-              h-[calc(100%+18px)] rounded-[2rem] bg-dark"
+              h-[calc(100%+18px)] rounded-[2rem] bg-dark dark:bg-light"
               />
               <SchillreffImage className="w-full h-auto rounded-2xl" />
             </div>
@@ -85,7 +88,7 @@ export default function About() {
                 <span className="inline-block text-7xl font-bold">
                   <AnimatedNumbers value={10} />+
                 </span>
-                <h2 className="text-xl font-medium capitalize text-dark/75">
+                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75">
                   satisfied clients
                 </h2>
               </div>
@@ -93,7 +96,7 @@ export default function About() {
                 <span className="inline-block text-7xl font-bold">
                   <AnimatedNumbers value={20} />+
                 </span>
-                <h2 className="text-xl font-medium capitalize text-dark/75">
+                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75">
                   complete projects
                 </h2>
               </div>
@@ -101,7 +104,7 @@ export default function About() {
                 <span className="inline-block text-7xl font-bold">
                   <AnimatedNumbers value={3} />+
                 </span>
-                <h2 className="text-xl font-medium capitalize text-dark/75">
+                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75">
                   years of experience
                 </h2>
               </div>
