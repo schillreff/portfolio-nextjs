1ecf15f9c16db22cc5c75f19643d6754719ebcf8
Leandro Schillreff <leandroschillreff@gmail.com>
2025-04-10 13:07:34 -0400

    style: standardize quotes in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  darkMode: "class",
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        dark: "#1b1b1b",
-        light: "#f5f5f5",
-        primary: "#B63E96",
-        primaryDark: "#58E6D9",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        dark: '#1b1b1b',
+        light: '#f5f5f5',
+        primary: '#B63E96',
+        primaryDark: '#58E6D9',
       },
       fontFamily: {
-        mont: ["var(--font-mont)"],
+        mont: ['var(--font-mont)'],
       },
       animation: {
-        "spin-slow": "spin 12s linear infinite",
+        'spin-slow': 'spin 12s linear infinite',
       },
       backgroundImage: {
         circularLight:
-          "repeating-radial-gradient(rgba(0,0,0,0.5) 2px, #F5F5F5 5px, #F5F5F5 100px);",
+          'repeating-radial-gradient(rgba(0,0,0,0.5) 2px, #F5F5F5 5px, #F5F5F5 100px);',
         circularDark:
-          "repeating-radial-gradient(rgba(255,255,255,0.5) 2px, #1b1b1b 5px, #1b1b1b 100px);",
+          'repeating-radial-gradient(rgba(255,255,255,0.5) 2px, #1b1b1b 5px, #1b1b1b 100px);',
       },
     },
   },
